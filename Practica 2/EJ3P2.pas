1f7ec5f2f4e2eceb3b893a7806dfc2919379b8a7
Laura Leguizamón <68202988+Lala-Lg@users.noreply.github.com>
2024-05-14 00:06:06 -0300

    Add files via upload

---
+++ Practica 2/EJ3P2.pas
@@ -0,0 +1,191 @@
+program EJ3P2;
+
+type
+  producto = record
+   codigoProducto: integer;
+   nombreComercial: string;
+   precioVenta: real;
+   stockActual:integer;
+   stockMinimo:integer;
+  end;
+  
+  ventaDiaria = record
+  codigoProducto:integer;
+  cantUnidadesVendidas:integer;
+  end;
+  
+  maestro = file of producto;
+  detalle = file of ventaDiaria;
+  
+  //Información sobre el producto que se almacene en la variable producto.
+  procedure leerProducto (var p:producto);
+  begin
+    with p do begin 
+        writeln('Productos');
+		write ('Ingrese codigo de producto: '); readln (codigoProducto);
+		if (codigoProducto <> -1) then begin
+			write ('Nombre comercial: '); readln (nombreComercial);
+			write ('Stock disponible: '); readln (stockActual);
+			write ('Stock minimo: '); readln (stockMinimo);
+			write ('Precio de venta: '); readln (precioVenta);
+		end;
+		writeln ('');
+	end;
+ end;
+ procedure leerVentaDiaria(var v:ventaDiaria);
+ begin
+   with v do begin
+     writeln('Venta diaria');
+     write ('Ingrese codigo de producto: '); readln (codigoProducto);
+		if (codigoProducto <> -1) then begin
+       write('Cantidad de unidades vendidas hoy: '); read(cantUnidadesVendidas);
+     end;
+   end;
+end;
+
+procedure crearMaestro(var m: maestro);
+var
+  p: producto;
+  nombre: string;
+begin
+  nombre := 'archivoMaestro';
+  assign(m,nombre);
+  rewrite(m); //Creo el archivo binario maestro.
+  reset(m);
+  writeln('Para dejar de ingresar productos, ingresar codigo de producto -1');
+  leerProducto(p);
+  while (p.codigoProducto <> -1) do begin
+    //Escribe el producto en el archivo binario maestro
+    write(m, p);
+    leerProducto(p);
+  end;
+  close(m); // Cierra el archivo binario maestro.
+end;
+
+procedure crearDetalle(var d:detalle);
+var
+  v:ventaDiaria;
+  nombre: string;
+begin
+  nombre := 'archivoDetalle';
+  assign(d,nombre);
+  rewrite(d);
+  reset(d);
+  writeln('Para dejar de ingresar las ventas diarias, ingresar codigo de producto -1');
+  leerVentaDiaria(v);
+  while(v.codigoProducto <> -1) do begin
+    write(d,v);
+    leerVentaDiaria(v);
+  end;
+  close(d);
+end;
+
+//Actualizar el archivo maestro con el archivo detalle.
+
+procedure actualizarMaestroConDetalle(var archivoMaestro: maestro; var archivoDetalle: detalle);
+var
+  productoMaestro: producto;
+  ventaDetalle: ventaDiaria;
+begin
+  reset(archivoMaestro); // Abrir el archivo maestro en modo lectura
+  reset(archivoDetalle); // Abrir el archivo detalle en modo lectura
+  while not eof(archivoMaestro) and not eof(archivoDetalle) do
+  begin
+    read(archivoMaestro, productoMaestro); // Leer un registro del archivo maestro
+    read(archivoDetalle, ventaDetalle); // Leer un registro del archivo detalle
+
+    if productoMaestro.codigoProducto = ventaDetalle.codigoProducto then
+    begin
+      // Actualizar el stock actual del producto en el archivo maestro
+      productoMaestro.stockActual := productoMaestro.stockActual - ventaDetalle.cantUnidadesVendidas;
+      // Volver al registro leído y escribir la actualización
+      seek(archivoMaestro, filepos(archivoMaestro) - 1);
+      write(archivoMaestro, productoMaestro);
+    end;
+  end;
+  close(archivoMaestro); // Cerrar el archivo maestro
+  close(archivoDetalle); // Cerrar el archivo detalle
+end;
+
+procedure imprimirDetalle(var archivoDetalle: detalle);
+var
+  v: ventaDiaria;
+begin
+  reset(archivoDetalle);
+  writeln('--- Archivo Detalle ---');
+  while not eof(archivoDetalle) do
+  begin
+    read(archivoDetalle, v);
+    writeln('Codigo de Producto: ', v.codigoProducto);
+    writeln('Cantidad de Unidades Vendidas: ', v.cantUnidadesVendidas);
+    writeln('');
+  end;
+  close(archivoDetalle);
+end;
+
+procedure imprimirMaestro(var archivoMaestro: maestro);
+var
+  p: producto;
+begin
+  reset(archivoMaestro);
+  writeln('--- Archivo Maestro ---');
+  while not eof(archivoMaestro) do
+  begin
+    read(archivoMaestro, p);
+    writeln('Codigo de Producto: ', p.codigoProducto);
+    writeln('Nombre Comercial: ', p.nombreComercial);
+    writeln('Precio de Venta: ', p.precioVenta:0:2);
+    writeln('Stock Actual: ', p.stockActual);
+    writeln('Stock Minimo: ', p.stockMinimo);
+    writeln('');
+  end;
+  close(archivoMaestro);
+end;
+
+//Listar en un archivo de texto llamado “stock_minimo.txt” aquellos productos cuyo stock actual esté por debajo del stock mínimo permitido.
+procedure crearTxt(var archivoTxt: Text);
+var
+  nombre: string;
+begin
+  nombre := 'stock_minimo.txt';
+  assign(archivoTxt, nombre);
+  rewrite(archivoTxt);
+end;
+
+//Para escribir en un txt hay que escribir campo por campo... F
+procedure escribirTxt(var archivoTxt: Text; p: producto);
+begin
+  writeln(archivoTxt, 'Codigo de Producto: ', p.codigoProducto);
+  writeln(archivoTxt, 'Nombre Comercial: ', p.nombreComercial);
+  writeln(archivoTxt, 'Precio de Venta: ', p.precioVenta:0:2);
+  writeln(archivoTxt, 'Stock Actual: ', p.stockActual);
+  writeln(archivoTxt, 'Stock Minimo: ', p.stockMinimo);
+  writeln(archivoTxt, '');
+end;
+
+procedure buscarProductos(var m: maestro; var archivoTxt: Text);
+var
+  p: producto;
+begin
+  reset(m); //Abro el archivo maestro para buscar entre los productos.
+  while not eof(m) do begin
+    read(m, p);
+    if (p.stockActual < p.stockMinimo) then
+      escribirTxt(archivoTxt, p);
+  end;
+  close(m);
+end;
+
+var m:maestro; d:detalle; archivoTxt:Text;
+begin
+
+  crearMaestro(m);
+  crearDetalle(d);
+  imprimirDetalle(d);
+  imprimirMaestro(m);
+  actualizarMaestroConDetalle(m,d);
+  imprimirMaestro(m);
+  crearTxt(archivoTxt);
+  buscarProductos(m,archivoTxt);
+  
+end.
